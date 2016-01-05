95afcf8c0611dd0c53057164849b9d7b8600a780
Chandu <cchhaanndduu@gmail.com>
2016-01-04 23:47:22 -0500

    Add C02

--- src/main/java/com/chandu/java8/practice/examples/C01.java
+++ src/main/java/com/chandu/java8/practice/examples/C01.java
@@ -1,9 +1,26 @@
 package com.chandu.java8.practice.examples;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 public class C01 implements Runnable {
+
+  public static boolean isPrime(final int number) {
+    return number > 1
+        && IntStream.range(2, number)
+            .noneMatch(i -> number % i == 0);
+  }
 
   @Override
   public void run() {
-    System.out.println("C01");
+    final List<String> sqrtOfFirst10Primes = Stream.iterate(1, n -> n + 1)
+        .filter(C01::isPrime)
+        .map(Math::sqrt)
+        .limit(10)
+        .map(n -> String.format("%.3f", n))
+        .collect(Collectors.toList());
+    System.out.println(sqrtOfFirst10Primes);
   }
 }
--- src/main/java/com/chandu/java8/practice/examples/C02.java
+++ src/main/java/com/chandu/java8/practice/examples/C02.java
@@ -1,9 +1,41 @@
 package com.chandu.java8.practice.examples;
 
+import java.util.function.Consumer;
+
 public class C02 implements Runnable {
 
   @Override
   public void run() {
-    System.out.println("C02");
+    Resource.consume(r -> {
+      r.op1();
+      r.op2();
+    });
+  }
+}
+
+class Resource {
+  private Resource() {
+    System.out.println("Initializing...");
+  }
+
+  public void op1() {
+    System.out.println("openration 1");
+  }
+
+  public void op2() {
+    System.out.println("openration 2");
+  }
+
+  private void close() {
+    System.out.println("Cleaning up...");
+  }
+
+  public static void consume(final Consumer<Resource> block) {
+    final Resource resource = new Resource();
+    try {
+      block.accept(resource);
+    } finally {
+      resource.close();
+    }
   }
 }
